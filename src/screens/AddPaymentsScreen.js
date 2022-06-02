1ccd9706fc42bda4620a438c868dd41a6bcee954
Dennis <basketor30@hotmail.fr>
2022-06-02 14:54:59 +0200

    creation squelette

---
+++ src/screens/AddPaymentsScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AddPaymentsScreen = () => {
+  return (
+    <View>
+      <Text>AddPaymentsScreen</Text>
+    </View>
+  )
+}
+
+export default AddPaymentsScreen
